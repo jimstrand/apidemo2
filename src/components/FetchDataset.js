d0db598862279c005e2ec88bffa3b81f4058d052
Jim Strand <jimstrand@Jims-MacBook-Pro.local>
2018-04-30 13:37:23 +0200

    API demo

--- src/components/FetchDataset.js
+++ src/components/FetchDataset.js
@@ -8,34 +8,14 @@ class FetchDataset extends Component {
 
   }
 
-  fetchformation(){
 
-    if(this.props.datasetid !== null){
-    var API_URL = 'https://app.oliasoft.com/api';
-    fetch(API_URL+'/evalds', {
-      method: 'POST',
-      body: JSON.stringify({email: 'EMAIL', password: 'PASSWORD'}),
-      headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify({
-        t: this.props.t,
-        dataset: this.props.datasetid
-      })
-    })
-    .then(res => res.json())
-    .then(json => {
-      console.log(json);
-
-    });
-  }
-
-}
 
 
   render(){
   return (
 
     <div>
-    hello yes hello {this.fetchformation()}
+
     </div>
 
   )
